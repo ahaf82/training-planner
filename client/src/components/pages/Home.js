4fb3bc16734588858d536ec73bb90043785bbb61
ahaf82 <a_h82@gmx.net>
2020-07-13 10:32:54 +0200

    Fixed Calendar

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -38,17 +38,19 @@ const Home = props => {
     return (
         <div className={`grid-${columns}`}>
             <div>
-                {(role === 'admin' || role === 'superUser') &&
-                    <h4 className="text-primary large center">Trainingseinheiten am:</h4>}
-                {(role === 'admin' || role === 'superUser') &&
-                    <SimpleReactCalendar activeMonth={new Date()} daysOfWeek={['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']} onSelect={(e) => {
-                        const actualDate = moment(e).format('YYYY-MM-DD');
-                        console.log(actualDate);
-                        filterTrainingSessions(actualDate);
-                    }} /> }
-                <br/>
-                {(role === 'admin' || role === 'superUser') &&
-                    <TrainingSessionFilter /> }
+                <div className='center-cal fixed'>
+                    {(role === 'admin' || role === 'superUser') &&
+                        <h4 className="text-primary large center">Trainingseinheiten am:</h4>}
+                    {(role === 'admin' || role === 'superUser') &&
+                        <SimpleReactCalendar activeMonth={new Date()} daysOfWeek={['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']} onSelect={(e) => {
+                            const actualDate = moment(e).format('YYYY-MM-DD');
+                            console.log(actualDate);
+                            filterTrainingSessions(actualDate);
+                        }} /> }
+                    <br/>
+                    {(role === 'admin' || role === 'superUser') &&
+                        <TrainingSessionFilter /> }
+                </div>
                 {role === 'member' &&
                     <TrainingSession /> }
                 {role === 'none' &&
