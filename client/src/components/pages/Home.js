c2974593d2cd10b0604eb16896779db3789e494d
ahaf82 <a_h82@gmx.net>
2020-07-13 09:44:11 +0200

    Fixed Calendar

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -38,21 +38,23 @@ const Home = props => {
     return (
         <div className={`grid-${columns}`}>
             <div>
-                {(role === 'admin' || role === 'superUser') &&
-                    <h4 className="text-dark large center">Trainingseinheiten am:</h4>}
-                {(role === 'admin' || role === 'superUser') &&
-                    <SimpleReactCalendar activeMonth={new Date()} daysOfWeek={['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']} onSelect={(e) => {
-                        const actualDate = moment(e).format('YYYY-MM-DD');
-                        console.log(actualDate);
-                        filterTrainingSessions(actualDate);
-                    }} /> }
-                <br/>
-                {(role === 'admin' || role === 'superUser') &&
-                    <TrainingSessionFilter /> }
-                {role === 'member' &&
-                    <TrainingSession /> }
-                {role === 'none' &&
-                    <h2 className="text-primary large">Melde dich bei deinem Trainer, damit er dich eincheckt</h2>}
+                <div className='fixed'>
+                    {(role === 'admin' || role === 'superUser') &&
+                        <h4 className="text-dark large center">Trainingseinheiten am:</h4>}
+                    {(role === 'admin' || role === 'superUser') &&
+                        <SimpleReactCalendar activeMonth={new Date()} daysOfWeek={['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So']} onSelect={(e) => {
+                            const actualDate = moment(e).format('YYYY-MM-DD');
+                            console.log(actualDate);
+                            filterTrainingSessions(actualDate);
+                        }} /> }
+                    <br/>
+                    {(role === 'admin' || role === 'superUser') &&
+                        <TrainingSessionFilter /> }
+                    {role === 'member' &&
+                        <TrainingSession /> }
+                    {role === 'none' &&
+                        <h2 className="text-primary large">Melde dich bei deinem Trainer, damit er dich eincheckt</h2>}
+                </div>
             </div>
             <div className='card-grid-3'>
                 {(role === 'admin' || role === 'superUser') &&
